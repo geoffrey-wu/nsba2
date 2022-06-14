25aa9b1eaa5e839e2465b8e86f125893b03130df
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-06-14 11:58:24 -0400

    fixed issues with result submission

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -3,22 +3,22 @@ document.getElementById('submit').addEventListener('click', () => {
     let away_players = {};
     for (let index = 1; index <= 7; index++) {
         home_players[document.getElementById(`player-${index}-home-name`).innerHTML] = {
-            tuh: document.getElementById(`player-${index}-home-tuh`).value,
-            points: document.getElementById(`player-${index}-home-points`).value,
+            tuh: parseInt(document.getElementById(`player-${index}-home-tuh`).value),
+            points: parseInt(document.getElementById(`player-${index}-home-points`).innerHTML),
             statline: [
-                document.getElementById(`player-${index}-home-4s`).value,
-                document.getElementById(`player-${index}-home-0s`).value,
-                document.getElementById(`player-${index}-home-negs`).value,
+                parseInt(document.getElementById(`player-${index}-home-4s`).value),
+                parseInt(document.getElementById(`player-${index}-home-0s`).value),
+                parseInt(document.getElementById(`player-${index}-home-negs`).value),
             ]
         };
 
         away_players[document.getElementById(`player-${index}-away-name`).innerHTML] = {
-            tuh: document.getElementById(`player-${index}-away-tuh`).value,
-            points: document.getElementById(`player-${index}-away-points`).value,
+            tuh: parseInt(document.getElementById(`player-${index}-away-tuh`).value),
+            points: parseInt(document.getElementById(`player-${index}-away-points`).innerHTML),
             statline: [
-                document.getElementById(`player-${index}-away-4s`).value,
-                document.getElementById(`player-${index}-away-0s`).value,
-                document.getElementById(`player-${index}-away-negs`).value,
+                parseInt(document.getElementById(`player-${index}-away-4s`).value),
+                parseInt(document.getElementById(`player-${index}-away-0s`).value),
+                parseInt(document.getElementById(`player-${index}-away-negs`).value),
             ]
         };
     }
@@ -33,18 +33,20 @@ document.getElementById('submit').addEventListener('click', () => {
         body: JSON.stringify({
             result: {
                 _id: 0, // calculated from `week_number*matchups.length + matchups.indexOf(this_game)`, but not guaranteed
-                week: document.getElementById('week').value,
+                week: parseInt(document.getElementById('week').value),
                 home: {
-                    name: document.getElementById('home-team-name').value,
+                    name: document.getElementById('home-team-name').innerHTML,
                     score: 0,
                     players: home_players
                 }, 
                 away: {
-                    name: document.getElementById('away-team-name').value,
+                    name: document.getElementById('away-team-name').innerHTML,
                     score: 0,
                     players: away_players
                 },
             }
         })
     })
+
+    document.getElementById('close').click();
 });
