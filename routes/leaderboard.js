230d185b3a6f66808c80cd7523a720211820b118
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-08 10:15:58 -0400

    added better leaderboard sorting and display tied ranks

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res, next) => {
         title: 'Leaderboard',
         username: req.session.username,
 
-        players: await database.getUsers(role = 'Player', {picture: 0}, {'stats.ppg': -1, 'stats.gp': -1, 'stats.tuh': 1})
+        players: await database.getUsers(role = 'Player', {picture: 0}, {'stats.ppg': -1, 'stats.pp22': -1, 'stats.gp': -1, 'stats.tuh': -1, 'stats.statline.0': -1, 'stats.statline.1': -1})
     });
 });
 
