e680f065279555ab36152752ba454870af97836d
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-20 14:36:09 -0400

    show staff profiles

--- routes/staff.js
+++ routes/staff.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+router.get(/\/.+/, async (req, res, next) => {
+    let staffName = decodeURI(req.url.substring(1));
+    if (staffName.charAt(staffName.length - 1) === '/')
+        staffName = staffName.substring(0, staffName.length - 1);
+
+    let player = await database.getUser(staffName, role = 'Admin', project = {});
+    if (player) {
+        res.render('user', {
+            title: staffName,
+            username: req.session.username,
+
+            results: await database.getResults(),
+            user: player
+        });
+    } else {
+        next(createError(404));
+    }
+});
+
 router.get('/', function (req, res, next) {
     res.render('staff', {
         title: 'Staff',
